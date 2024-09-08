4c616667e839a6f765d8ea70dc08d4a3dcaa6a8d
RudySeo <gun100491@gmail.com>
2024-09-08 12:52:33 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/product/domain/Product.kt
@@ -0,0 +1,14 @@
+package com.example.product.domain
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+data class Product(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    var name: String,
+    var price: Double
+)
